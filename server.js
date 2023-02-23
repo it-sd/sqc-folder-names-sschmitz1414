ba932d6a91f09c1094d6a1c62cbf6ec4dd237d57
Samuel Schmitz <sschmitz14@student.cvtc.edu>
2023-02-22 20:27:43 -0600

    git commit 'Add new folders correctly'

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 const newFolderName = function (folders) {
+  let i = 2;
+    let result = "";
 
+    if (folders.includes("New Folder")) {
+        while (i <= (folders.length + 1)) {
+            if (folders.includes("New Folder (" + i + ")")) {
+                i++
+            }
+            else {
+            result = "New Folder (" + i + ")";
+            break;
+            }
+        }
+        
+    }
+    else {
+        result = "New Folder";
+    }
+
+    return result;
 }
+
+console.log(newFolderName([""]))
+
+console.log(newFolderName(["New Folder", "New Folder (2)"]))
+
+module.exports = { newFolderName }
